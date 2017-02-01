8cd914a4763a52153cf0090ae6d3d8b4bc2a6f72
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-02 01:40:01 +0530

    Added updating on scroll bar when pbar clicked

--- js/player.js
+++ js/player.js
@@ -1,26 +1,68 @@
 // window load event listener
 window.addEventListener('load', () => {
-    var video = document.getElementById('video');
-    // play or pause event for video player on play button click
-    var playButton = document.getElementById('play-button');
+    video = document.getElementById('video');
+    
+    // Get the elements of the form as global variables
+    playButton = document.getElementById('play-button');
+    fullscreenButton = document.getElementById('fullscreen-button');
+    pbar = document.getElementById('pbar');
+    pbarContainer = document.getElementById('pbar-container');
+    soundButton = document.getElementById('sound-button');
+    timeField = document.getElementById('time-field');
 
     video.load();
     // canplay event listener for video
     video.addEventListener('canplay', function() {
         // event listener for when the play button is clicked. 
         playButton.addEventListener('click', playOrPause, false);
+        // progress bar click
+        pbarContainer.addEventListener('click', skip, false);
+        // button to make the video fullscreen
+        fullscreenButton.addEventListener('click', fullscreenOrNot, false);
+        // Toggle mute
+        // soundButton.addEventListener('click', muteOrNot, false);
     }, false);
 
 }, false);
 
+// play or pause on button click
 var playOrPause = () => {
-    var playButton = document.getElementById('play-button');
-
     if (video.paused){
         video.play();
         playButton.src = 'media/pause.png';
+        // run every 30 ms
+        update = setInterval(updatePlayer, 30); 
     } else {
         video.pause();
         playButton.src = 'media/play.png';
+        // interval back to zero
+        window.clearInterval(update);
     }
+};
+
+// modify screen size on button click
+var fullscreenOrNot = () => {
+    // Enter full screen if screen is not full screen
+    // Enter normal screen if screen is full screen
+};
+
+var updatePlayer = () => {
+    var percentage = (video.currentTime / video.duration) * 100;
+
+    pbar.style.width = percentage + '%';
+
+    if (video.ended){
+        window.clearInterval(update);
+        playButton.src = 'media/replay.png';
+    }
+};
+
+var skip = (event) => {
+    // x-coord of mouse
+    var mousex = event.pageX - pbarContainer.offsetLeft; 
+    // width of pbar = 640px
+    var barwidth = window.getComputedStyle(pbarContainer).getPropertyValue('width'); 
+    var width = barwidth.substr(0, barwidth.length - 2);
+
+    video.currentTime = (mousex / width) * video.duration;
 };
