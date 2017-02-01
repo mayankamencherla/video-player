e20d6dc7881eaefcda66844508d740ec66c89789
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-01 22:59:53 +0530

    Added event listener for video loading

--- js/player.js
+++ js/player.js
@@ -1,17 +1,26 @@
 // window load event listener
 window.addEventListener('load', () => {
     var video = document.getElementById('video');
-
     // play or pause event for video player on play button click
     var playButton = document.getElementById('play-button');
-    playButton.addEventListener('click', playOrPause, false);
+
+    video.load();
+    // canplay event listener for video
+    video.addEventListener('canplay', function() {
+        // event listener for when the play button is clicked. 
+        playButton.addEventListener('click', playOrPause, false);
+    }, false);
 
 }, false);
 
 var playOrPause = () => {
+    var playButton = document.getElementById('play-button');
+
     if (video.paused){
         video.play();
+        playButton.src = 'media/pause.png';
     } else {
         video.pause();
+        playButton.src = 'media/play.png';
     }
 };
