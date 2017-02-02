01fa7be791449f4bbe88924df68131a5e315f378
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-02 10:50:44 +0530

    Formatted video duration div

--- js/player.js
+++ js/player.js
@@ -13,6 +13,7 @@ window.addEventListener('load', () => {
     video.load();
     // canplay event listener for video
     video.addEventListener('canplay', function() {
+        updatePlayer();
         // event listener for when the play button is clicked. 
         playButton.addEventListener('click', playOrPause, false);
         // progress bar click
@@ -44,12 +45,16 @@ var playOrPause = () => {
 var fullscreenOrNot = () => {
     // Enter full screen if screen is not full screen
     // Enter normal screen if screen is full screen
+    video.fullscreen();
 };
 
 var updatePlayer = () => {
     var percentage = (video.currentTime / video.duration) * 100;
-
     pbar.style.width = percentage + '%';
+
+    var currentTime = getFormattedTime(video.currentTime);
+    var duration = getFormattedTime(video.duration)
+    timeField.innerHTML = currentTime + ' / ' + duration;
 
     if (video.ended){
         window.clearInterval(update);
@@ -62,7 +67,29 @@ var skip = (event) => {
     var mousex = event.pageX - pbarContainer.offsetLeft; 
     // width of pbar = 640px
     var barwidth = window.getComputedStyle(pbarContainer).getPropertyValue('width'); 
-    var width = barwidth.substr(0, barwidth.length - 2);
+    // width of pbar = 640
+    var width = parseFloat(barwidth.substr(0, barwidth.length - 2));
 
     video.currentTime = (mousex / width) * video.duration;
+    updatePlayer();
+};
+
+// MM:SS / MM:SS
+var getFormattedTime = (time) => {
+    var seconds = Math.round(time);
+
+    var minutes = Math.floor(seconds / 60);
+
+    // To avoid second number 61, 62, 63 etc
+    seconds = seconds - minutes * 60;
+
+    if (seconds.toString().length === 1){
+        seconds = '0' + seconds;
+    }
+
+    if (minutes.toString().length === 1){
+        minutes = '0' + minutes;
+    }
+
+    return minutes + ':' + seconds;
 };
